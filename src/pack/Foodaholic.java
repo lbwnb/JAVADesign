8bef6496f4e9d26fb174bd01cf6410d5dadab364
lbwnb <61997011@qq.com>
2018-07-19 20:44:44 +0800

    抽象工厂模式 产品等级结构：比如一个抽象类是食物，其子类有苹果、牛奶等等，则抽象食物与具体食物名称之间构成了一个产品等级结构。食物是抽象的父类，而具体的食物名称是其子类。
    产品族：在抽象工厂模式中，产品族是指由同一个工厂生产的，位于不同产品等级结构中的一组产品。如 AKitchen
    生产的苹果、刀子，苹果属于食物产品等级结构中，而刀子则属于餐具产品等级结构中。而 BKitchen 可能生成另一组产品，如牛奶、杯子。
    
    因此工厂方法模式、抽象工厂模式最大的区别在于：
    
    工厂方法模式：针对的是 一个产品等级结构。
    
    抽象工厂模式：针对 多个产品等级结构。

---
+++ src/pack/Foodaholic.java
@@ -0,0 +1,60 @@
+package pack;
+
+//抽象食物
+interface Food{
+	public String getFoodName();
+}
+
+//抽象餐具
+interface TableWare{
+	public String getToolName();
+}
+
+//抽象工厂
+interface KitchenFactory{
+	public Food getFood();
+	public TableWare getTableWare();
+}
+
+//具体食物Apple的定义如下
+class Apple implements Food{
+	public String getFoodName() {
+		return "apple";
+	}
+}
+
+//具体餐具Knife的定义如下
+class Knife implements TableWare{
+	public String getToolName() {
+		return "knife";
+	}
+	
+}
+
+//以具体工厂AKitchen为例
+class AKitchen implements KitchenFactory{
+
+	@Override
+	public Food getFood() {
+		// TODO Auto-generated method stub
+		return new Apple();
+	}
+
+	@Override
+	public TableWare getTableWare() {
+		// TODO Auto-generated method stub
+		return new Knife();
+	}
+	
+}
+public class Foodaholic {
+	
+	public void eat(KitchenFactory k) {
+		System.out.println("A foodaholic is eating" + k.getFood().getFoodName() + " with " + k.getTableWare().getToolName() );
+	}
+   public static void main(String[] args) {
+	   Foodaholic fh = new Foodaholic();
+	   KitchenFactory kf = new AKitchen();
+	   fh.eat(kf);
+   }
+}
